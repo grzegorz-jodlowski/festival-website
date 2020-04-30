2dd8a379326914e9e0c05de7951fb74adfa8214e
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-30 17:26:15 +0200

    Add middleware to server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 const port = 8000
 
 app.get('/', (req, res) => res.send('Hello World!'))
