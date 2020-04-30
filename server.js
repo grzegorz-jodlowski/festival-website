4068f424bb4de45b279192042370b5b8e381e8e2
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-04-30 17:50:06 +0200

    Add get endpoints

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const port = 8000
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+];
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = 8000;
+
+app.get('/testimonials', (req, res) => res.send(db));
+app.get('/testimonials/:id', (req, res) => res.send(db[req.params.id]));
+app.get('/testimonials/random', (req, res) => res.send(db[Math.floor(Math.random() * (db.length))]));
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
