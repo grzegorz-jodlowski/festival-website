09cc4cecdd624d39a94f135af1854cd9c01d871c
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-06 15:41:03 +0200

    Add box with amount of free seats

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -39,7 +39,8 @@ class SeatChooser extends React.Component {
   render() {
 
     const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { requests, seats } = this.props;
+    console.log(' : SeatChooser -> render -> seats', seats);
 
     return (
       <div>
@@ -49,6 +50,7 @@ class SeatChooser extends React.Component {
         {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i + 1))}</div>}
         {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} />}
         {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert>}
+        <small id="freeSeats" className="form-text text-muted ml-2">Free seats: {`${150 - seats.length}/150`}</small>
       </div>
     )
   };
