2a3c26c9cb8aa9c0c70388cf4c38cb534216d934
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-11 19:12:22 +0200

    Remove id from models (leave _id from mongo)

--- models/testimonial.model.js
+++ models/testimonial.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const testimonialSchema = new mongoose.Schema({
-  id: { type: String, required: true },
   author: { type: String, required: true },
   text: { type: String, required: true }
 });
