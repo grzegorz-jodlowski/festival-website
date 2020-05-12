1d472ecf378c1c03046004d1d454187de85be7fd
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-12 15:59:30 +0200

    Refactor concerts endpoints to mongoDB

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,35 +1,85 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
-
 const router = express.Router();
-const db = require('./../db');
 
-router.route('/concerts').get((req, res) => res.json(db.concerts));
-router.route('/concerts/random').get((req, res) => res.json(db.concerts[Math.floor(Math.random() * (db.concerts.length))]));
-router.route('/concerts/:id').get((req, res) => res.json(db.concerts.find(el => el.id == req.params.id)));
+const Concert = require('../models/concert.model');
 
-router.route('/concerts').post((req, res) => {
-  const { performer } = req.body;
+router.get('/concerts', async (req, res) => {
+  try {
+    res.json(await Concert.find())
+  } catch (error) {
+    res.json({ message: error })
+  }
+});
 
-  if (performer) {
-    db.concerts.push({ ...req.body, id: uuidv4() })
+router.get('/concerts/random', async (req, res) => {
+  try {
+    const count = await Concert.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const randomConcert = await Concert.findOne().skip(rand);
+    if (!randomConcert) res.status(404).json({ message: 'Not found' });
+    else res.json(randomConcert);
+  }
+  catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get('/concerts/:id', async (req, res) => {
+  try {
+    const concert = await Concert.findById(req.params.id);
+    if (!concert) res.status(404).json({ message: 'Not found' });
+    else res.json(concert);
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+});
+
+router.post('/concerts', async (req, res) => {
+  try {
+    const { performer, genre, price, day, image } = req.body;
+    const newConcert = new Concert({ performer, genre, price, day, image });
+    await newConcert.save()
     res.json({ message: 'OK' });
-  }
-  else {
-    res.json({ message: 'You can\'t leave performer field empty!' })
+  } catch (error) {
+    res.status(500).json({ message: error });
   }
 });
 
-router.route('/concerts/:id').put((req, res) => {
-  const dbRecord = db.concerts.find(el => el.id == req.params.id)
-  db.concerts.splice(db.concerts.indexOf(dbRecord), 1, { ...dbRecord, ...req.body });
-  res.json({ message: 'OK' });
+router.put('/concerts/:id', async (req, res) => {
+  try {
+    const { performer, genre, price, day, image } = req.body;
+
+    const updatedElement = {};
+    performer ? updatedElement.performer = performer : null;
+    genre ? updatedElement.genre = genre : null;
+    price ? updatedElement.price = price : null;
+    day ? updatedElement.day = day : null;
+    image ? updatedElement.image = image : null;
+
+    const concert = await Concert.findById(req.params.id);
+    if (concert) {
+      await Concert.updateOne({ _id: req.params.id }, { $set: updatedElement })
+      res.json(await Concert.findById(req.params.id));
+    } else {
+      res.status(404).json({ message: 'Not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 });
 
-router.route('/concerts/:id').delete((req, res) => {
-  const dbRecord = db.concerts.find(el => el.id == req.params.id)
-  db.concerts.splice(db.concerts.indexOf(dbRecord), 1);
-  res.json({ message: 'OK' });
+router.delete('/concerts/:id', async (req, res) => {
+  try {
+    const concert = await Concert.findById(req.params.id);
+    if (concert) {
+      await Concert.deleteOne({ _id: req.params.id });
+      res.json(concert);
+    } else {
+      res.status(404).json({ message: error });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
 });
 
 module.exports = router;
