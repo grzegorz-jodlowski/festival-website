e351718be234412371144fcfb90e24a93a2e6e7c
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-15 18:43:07 +0200

    Add post endpoint for /workshops

--- controllers/workshops.controller.js
+++ controllers/workshops.controller.js
@@ -13,9 +13,9 @@ exports.getRandom = async (req, res) => {
   try {
     const count = await Workshop.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const randomWorkchop = await Workshop.findOne().skip(rand);
-    if (!randomWorkchop) res.status(404).json({ message: 'Not found' });
-    else res.json(randomWorkchop);
+    const randomWorkshop = await Workshop.findOne().skip(rand);
+    if (!randomWorkshop) res.status(404).json({ message: 'Not found' });
+    else res.json(randomWorkshop);
   }
   catch (error) {
     res.status(500).json({ message: error });
@@ -31,3 +31,15 @@ exports.getById = async (req, res) => {
     res.status(500).json({ message: error })
   }
 }
+
+exports.postWorkshop = async (req, res) => {
+  try {
+    const { name, concertId } = req.body;
+    const newWorkshop = new Workshop({ name, concertId });
+    await newWorkshop.save()
+    res.json({ message: 'OK' });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+}
+
