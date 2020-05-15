4b1d5b987f584d8ecc29a2dc41ee3a907b8d2709
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-15 19:22:32 +0200

    Add first test for workshop model

--- models/test/workshop.test.js
+++ models/test/workshop.test.js
@@ -1,4 +1,13 @@
 const expect = require('chai').expect;
+const mongoose = require('mongoose');
 
 const Workshop = require('../workshop.model');
 
+it('should throw an error if no args', () => {
+  const dep = new Workshop({});
+
+  dep.validate(err => {
+    expect(err.errors.name).to.exist;
+  });
+
+});
