84c96d5dcba0c09d397e879f4c9bf627bb6b4c35
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-16 14:54:24 +0200

    Add tests for workshop model

--- models/test/workshop.test.js
+++ models/test/workshop.test.js
@@ -3,11 +3,38 @@ const mongoose = require('mongoose');
 
 const Workshop = require('../workshop.model');
 
-it('should throw an error if no args', () => {
-  const dep = new Workshop({});
 
-  dep.validate(err => {
-    expect(err.errors.name).to.exist;
+describe('Workshop', () => {
+  const args = ['name', 'concertId'];
+
+  it('should throw an error if no args', () => {
+    const workshop = new Workshop({});
+
+    workshop.validate(err => {
+      args.forEach(arg => {
+        expect(err.errors[arg]).to.exist;
+      })
+    });
   });
 
+  it('should throw an error if "name" and "concertId are not strings', () => {
+    const cases = [{}, []];
+
+    for (let element of cases) {
+      const workshop = new Workshop({ name: element, concertId: element });
+      workshop.validate(err => {
+        args.forEach(arg => {
+          expect(err.errors[arg]).to.exist;
+        })
+      });
+    }
+  });
+
+  it('should not throw an error if args have proper format', () => {
+    const workshop = new Workshop({ name: 'Workshop #1', concertId: 'Concert #1' });
+
+    workshop.validate(err => {
+      expect(err).to.not.exist;
+    });
+  });
 });
